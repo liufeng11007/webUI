78e70453199ea81c8212cc40be95f3ab0014cfb9
liufeng11007 <80759866@qq.com>
2021-01-26 11:26:24 +0800

    只执行公海的测试用例

--- run.py
+++ run.py
@@ -12,6 +12,6 @@ REPORT_FILE = os.path.join(REPORT_PATH, "demo_report.html")
 
 if __name__ == '__main__':
     # pytest.main(["-m xiansuo", f"--html={REPORT_FILE}", "--alluredir=reports/"])
-    pytest.main(["-m login", "--alluredir=reports/"])
+    pytest.main(["-m gonghai", "--alluredir=reports/"])
 
 
