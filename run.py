252ed4d21c9edca17846d49c8d0e4ef4d9a4b5a9
liufeng11007 <80759866@qq.com>
2021-01-26 16:37:14 +0800

    执行login测试用例

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ REPORT_FILE = os.path.join(REPORT_PATH, "demo_report.html")
 
 if __name__ == '__main__':
     # pytest.main(["-m xiansuo", f"--html={REPORT_FILE}", "--alluredir=reports/"])
-    pytest.main(["-m staging", "--alluredir=reports/"])
+    pytest.main(["-m login", "--alluredir=reports/"])
 
 
 
